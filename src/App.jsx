ec6a718f6bce0801c228f606b602aa2e31b264ab
psp <976477689@qq.com>
2022-07-26 21:01:06 +0800

    hava a try

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
 import { Menu } from 'antd'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.scss'
 import One from './pages/One'
 import Two from './pages/Two'
 import Three from './pages/Three'
 import Four from './pages/Four'
 
-const MenuItemList = [
-  {
-    label: '菜单一',
-    key: 1
-  },
-  {
-    label: '菜单二',
-    key: 2
-  },
-  {
-    label: '菜单三',
-    key: 3
-  },
-  {
-    label: '菜单四',
-    key: 4
-  }
-]
 function App() {
+  const menuItemList = [
+    {
+      label: '菜单一',
+      key: '/',
+      link: '/'
+    },
+    {
+      label: '菜单二',
+      key: '/two',
+      link: '/two'
+    },
+    {
+      label: '菜单三',
+      key: '/three',
+      link: '/three'
+    },
+    {
+      label: '菜单四',
+      key: '/four',
+      link: '/four'
+    }
+  ]
+  const navigate = useNavigate()
+  const clickMenuItem = (e) => {
+    navigate(e.key)
+  }
   return (
     <div className="main">
       <div className="left">
-        <Menu items={MenuItemList} />
+        <Menu items={menuItemList} onClick={e => clickMenuItem(e)} />
       </div>
       <div className="right">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/one" element={<One />}></Route>
-            <Route path="/two" element={<Two />}></Route>
-            <Route path="/three" element={<Three />}></Route>
-            <Route path="/four" element={<Four />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route index element={<One />}></Route>
+          <Route path="/two" element={<Two />}></Route>
+          <Route path="/three" element={<Three />}></Route>
+          <Route path="/four" element={<Four />}></Route>
+        </Routes>
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import 'antd/dist/antd.css';
 import './assets/css/reset.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 )
